e1e87723709199185bd46e763fa4e236b5b52e5d
David <witche3@googlemail.com>
2016-03-05 21:25:55 +0100

    disqus

--- angular/directives/footer.js
+++ angular/directives/footer.js
@@ -10,7 +10,7 @@
             link: function ($scope) {
                 $scope.disqusConfig = {
                     disqus_shortname: "raspberrypiledstrip",
-                    disqus_identifier: "http://popoklopsi.github.io/RaspberryPi-LedStrip"
+                    disqus_identifier: "http://popoklopsi.github.io/RaspberryPi-LedStrip",
                     disqus_url: "http://popoklopsi.github.io/RaspberryPi-LedStrip/#/"
                 };
             }
