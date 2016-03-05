f1d14543ffeda5d2036e4a42c78e3d4a2973f798
David <witche3@googlemail.com>
2016-03-05 18:53:36 +0100

    Adding disqus directive

--- angular/directives/footer.js
+++ angular/directives/footer.js
@@ -7,18 +7,12 @@
             replace: "true",
             templateUrl: "angular/directives/footer.tpl.html",
             scope: {},
-            link: function () {
-                var disqus_config = function () {
-                    this.page.url = "http://popoklopsi.github.io/RaspberryPi-LedStrip/#!/";
+            link: function ($scope) {
+                $scope.disqusConfig = {
+                    disqus_shortname: "raspberrypiledstrip",
+                    disqus_identifier: "common",
+                    disqus_url: "http://popoklopsi.github.io/RaspberryPi-LedStrip/#!/"
                 };
-                
-                var dsq = document.createElement("script");
-
-                dsq.type = "text/javascript";
-                dsq.async = true;
-                dsq.src = "//raspberrypiledstrip.disqus.com/embed.js";
-
-                (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);
             }
         }
     }]);
