5cf8836c4c0db56aa66ae5594d22ec43785bf86a
David <witche3@googlemail.com>
2016-03-05 21:07:58 +0100

    Testing disqus directive again

--- angular/directives/footer.js
+++ angular/directives/footer.js
@@ -8,13 +8,10 @@
             templateUrl: "angular/directives/footer.tpl.html",
             scope: {},
             link: function ($scope) {
-                var dsq = document.createElement("script");
-
-                dsq.type = "text/javascript";
-                dsq.async = true;
-                dsq.src = "//raspberrypiledstrip.disqus.com/embed.js";
-
-                (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(dsq);
+                $scope.disqusConfig = {
+                    disqus_shortname: "raspberrypiledstrip",
+                    disqus_url: "http://popoklopsi.github.io/RaspberryPi-LedStrip/#/"
+                };
             }
         }
     }]);
